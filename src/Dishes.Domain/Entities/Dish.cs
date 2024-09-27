91b0fbecb79fca767243ef8d9c98e9d6bd0e2878
vnribeiro <viniciuscostaa.ribeiro@outlook.com>
2024-09-26 21:31:42 -0300

    Initial Commit

---
+++ src/Dishes.Domain/Entities/Dish.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dishes.Domain.Entities;
+
+public class Dish 
+{ 
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Name { get; set; }
+
+    public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+
+    public Dish() {}
+
+    [SetsRequiredMembers]
+    public Dish(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
